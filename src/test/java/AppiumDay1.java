1034659546f872ff4c34f483c3a0a9be4449952b
farukyucel5 <117035308+farukyucel5@users.noreply.github.com>
2023-02-19 15:55:45 +0300

    MobiletestWithAppium

---
+++ src/test/java/AppiumDay1.java
@@ -0,0 +1,37 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppiumDay1 {
+
+    AndroidDriver<AndroidElement> driver;
+
+    @BeforeMethod
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities=new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName","Android");
+        desiredCapabilities.setCapability("platformVersion","9.0");
+        desiredCapabilities.setCapability("deviceName","Pixel_2");
+        desiredCapabilities.setCapability("automationName","UiAutomator2");
+        desiredCapabilities.setCapability("app","C:\\Asoftware\\AppiumProject1\\src\\Apps\\Calculator.apk");
+        driver= new AndroidDriver<AndroidElement>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
+    }
+    @Test
+    public void app() throws MalformedURLException {
+
+        driver.findElementById("com.google.android.calculator:id/digit_9").click();
+        driver.findElementById("com.google.android.calculator:id/op_mul").click();
+        driver.findElementById("com.google.android.calculator:id/digit_8").click();
+        AndroidElement result= driver.findElementById("com.google.android.calculator:id/result_preview");
+        System.out.println(result.getText());
+        Assert.assertEquals(result.getText(), "72");
+    }
+}
