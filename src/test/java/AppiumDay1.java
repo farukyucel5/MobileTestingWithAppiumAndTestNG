3e94bfd6a5c8e655b60f11230c40c38de8de549a
farukyucel5 <117035308+farukyucel5@users.noreply.github.com>
2023-02-20 18:22:20 +0300

    Updating

--- src/test/java/AppiumDay1.java
+++ src/test/java/AppiumDay1.java
@@ -1,4 +1,4 @@
-import io.appium.java_client.MobileElement;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
